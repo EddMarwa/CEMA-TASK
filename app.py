3e8dfcd8585fbe6bb53c612c232544e2066273d0
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-24 12:41:19 +0300

    database

--- app.py
+++ app.py
@@ -25,3 +25,28 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+################ A. Create a Health Program ##############################
+@app.route('/create_program', methods=['POST'])
+def create_program():
+    data = request.get_json()
+    programs = read_json(PROGRAMS_FILE)
+    programs.append(data)
+    write_json(programs, PROGRAMS_FILE)
+    return jsonify({"message": "Program created!"})
+################# B. Register a Client ################################
+@app.route('/register_client', methods=['POST'])
+def register_client():
+    client_data = request.get_json()
+    clients = read_json(CLIENTS_FILE)
+    clients.append(client_data)
+    write_json(clients, CLIENTS_FILE)
+    return jsonify({"message": "Client registered!"})
+
+################# C. Search for a Client ################################
+@app.route('/search_client/<name>')
+def search_client(name):
+    clients = read_json(CLIENTS_FILE)
+    for client in clients:
+        if client['name'].lower() == name.lower():
+            return jsonify(client)
+    return jsonify({"message": "Client not found"})
