80baae54c2b1134ddf41028d3334507366c558e4
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-26 20:02:31 +0300

    app side

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
-# InitialiFlask App
+# Initiali Flask App
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
