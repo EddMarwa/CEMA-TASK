d65203b6eb8e6ee58f411973c20cf3335c3d7f65
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-29 00:26:13 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -90,7 +90,6 @@ def home():
 @login_required
 def search():
     return render_template('search.html')
-
 #------------------- Health Program & Client Routes -------------------#
 @app.route('/create_program', methods=['POST'])
 @login_required
