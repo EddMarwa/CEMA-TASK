3550ce9e54d680e693aa63e2c20886eccf7c79dd
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-28 22:25:58 +0300

    ls rf

--- app.py
+++ app.py
@@ -80,8 +80,6 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 #------------------- Protected Routes -------------------#
-
-
 @app.route('/home')
 @login_required
 def home():
