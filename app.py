a1029e751caabeca76013fcabd4ddd17aabd3871
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-05-04 00:12:12 +0300

    ytr

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def signup():
             flash("Username already exists.")
             return redirect(url_for('signup'))
 
-        # Hashing passwords before storage in database
+        # Hashing passwords storage in database
         hashed_password = generate_password_hash(password)
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
