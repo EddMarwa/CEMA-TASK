e70ff8e3a9065d8c1abbaef7957af4ddf81428dc
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-05-04 15:16:18 +0300

    Flask

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_limiter.util import get_remote_address
 import re
 import os
 
-# FlaskInitializing first phase 
+# Initializing first phase 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
