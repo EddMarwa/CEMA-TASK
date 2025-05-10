d9bed50e601fd2cfdaf17d4157c5d34bc7d86d92
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-05-10 05:16:24 +0300

    cmt

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def create_program():
 def register_client():
     client_name = request.form.get('name')
     
-    # Create new clients
+    # Creates new clients
     new_client = Client(name=client_name)
     db.session.add(new_client)
     db.session.commit()
