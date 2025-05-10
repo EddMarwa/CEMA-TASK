33e3daba5bede180dbfc9a9da302a2e0b24ec40d
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-05-10 05:22:15 +0300

    db models

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-
 #---------------- Database Models ------------------#
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
