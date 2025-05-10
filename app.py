075f4def1e11faedc02dd60de3798409f9f5a43a
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-05-10 05:17:40 +0300

    timer

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def index():
     return redirect(url_for('login'))
 
 
-# Initializig  Limiters for trials
+# Initializig  Limiters
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
