e58ec3b12185d943bf9eb8874188cc85a89540f1
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-05-21 12:23:34 +0300

    Y

--- app.py
+++ app.py
@@ -242,7 +242,6 @@ def delete_program(program_id):
 def create_tables():
     with app.app_context():
         db.create_all()
-
 if __name__ == '__main__':
     create_tables()
     app.run(debug=True)
